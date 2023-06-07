81ec1c1cf16497e274046db19599f34287e8c1d4
caishan <helongjiang0085@dingtalk.com>
2023-06-07 09:13:59 +0800

    初始化

---
+++ fuxiaotong-interview/src/main/java/com/example/fuxiaotonginterview/FuxiaotongInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuxiaotonginterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuxiaotongInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuxiaotongInterviewApplication.class, args);
+    }
+
+}
---
+++ fuxiaotong-interview/src/test/java/com/example/fuxiaotonginterview/FuxiaotongInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuxiaotonginterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuxiaotongInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
